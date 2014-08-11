81461ec506aa4801971e9420068ab51cc75264fc
sergiu.girnet <sergiu.girnet@osf-global.com>
2014-08-11 16:13:07 +0300

    WebHooks notifications implementation

---
+++ CTCTWrapper/Webohooks/Helper/StringValueAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CTCT.Webohooks.Helper
+{
+    #region Class StringValueAttribute
+
+    /// <summary>
+    /// Simple attribute class for storing String Values
+    /// </summary>
+    public class StringValueAttribute : Attribute
+    {
+        private string _value;
+
+        /// <summary>
+        /// Creates a new <see cref="StringValueAttribute"/> instance.
+        /// </summary>
+        /// <param name="value">Value.</param>
+        public StringValueAttribute(string value)
+        {
+            _value = value;
+        }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value></value>
+        public string Value
+        {
+            get { return _value; }
+        }
+    }
+
+    #endregion
+}
---
+++ CTCTWrapper/Webohooks/Model/BillingChangeNotification.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Collections;
+using CTCT.Webohooks.Helper;
+using CTCT.Components;
+using System.Runtime.Serialization;
+
+namespace CTCT.Webohooks.Model
+{
+    /// <summary>
+    /// Represents a notification object sent to client through Webhooks Notifications.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class BillingChangeNotification : Component
+    {
+        #region Propertiese
+
+        /// <summary>
+        /// Notification url
+        /// </summary>
+        [DataMember(Name = "url", EmitDefaultValue = false)]
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Event type string
+        /// </summary>
+        [DataMember(Name = "event_type", EmitDefaultValue = false)]
+        private string EventTypeString { get; set; }
+
+        /// <summary>
+        /// Event type
+        /// </summary>
+        public BillingChangeNotificationType EventType
+        {
+            get
+            {
+                return (BillingChangeNotificationType)StringEnum.Parse(typeof(BillingChangeNotificationType), EventTypeString, true);
+            }
+            set
+            {
+                EventTypeString = StringEnum.GetStringValue(value);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// ToString override
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("BillingChangeNotification [url=");
+            builder.Append(Url);
+            builder.Append(", event_type=");
+            builder.Append(EventType);
+            builder.Append("]");
+
+            return builder.ToString();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Event types for billing change notification.
+    /// </summary>
+    public enum BillingChangeNotificationType
+    {
+        [StringValue("tier.increase")]
+        TierIncrease,
+
+        [StringValue("tier.decrease")]
+        TierDecrease
+    }
+}
